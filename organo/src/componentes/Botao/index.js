6ec39e153b40fdf7366f2a307d8d48aac8bdedac
glaubert.almeida <glaubealmeida@hotmail.com>
2026-07-02 12:17:11 -0300

    Teste

--- organo/src/componentes/Botao/index.js
+++ organo/src/componentes/Botao/index.js
@@ -3,8 +3,10 @@ import './Botao.css'
 const Botao = (props) => {
     return (
     <button className='botao'>
-        //props.children permite passar o conteúdo encapsulado do pai para o filho.
+        {/* Passa o props escrito no Pai para o filho */}
         {props.children}
     </button>
     )
 }
+
+export default Botao
